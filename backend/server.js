b35b318d7851b85eb1be0aa88fd0369351a1ffbb
Onelio Viera <ojvwebdesign@gmail.com>
2023-07-29 22:28:41 -0400

    commit

--- backend/server.js
+++ backend/server.js
@@ -33,7 +33,6 @@ app.get('/api/config/paypal', (req, res) =>
 res.send({ clientId: process.env.PAYPAL_CLIENT_ID })
 );
 
-
 app.use(notFound);
 app.use(errorHandler);
 
--- frontend/src/slices/ordersApiSlice.js
+++ frontend/src/slices/ordersApiSlice.js
@@ -29,7 +29,20 @@ export const ordersApiSlice = apiSlice.injectEndpoints({
       }), 
       keepUnusedDataFor: 5,
     }),
+    payOrder: builder.mutation({
+      query: ({ orderId, details }) => ({
+        url: `${ORDERS_URL}/${orderId}/pay`,
+        method: 'PUT',
+        body: { ...details },
+      }), 
+    }),
+    getPayPalClientId: builder.query({
+      query: () => ({
+        url: PAYPAL_URL,
+    }),
+    keepUnusedDataFor: 5,
   }),
+}),
 });
 
 export const { 
