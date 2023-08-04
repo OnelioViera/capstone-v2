540a01b8419aae8550cffdbe4e75d77d04eba116
Onelio Viera <ojvwebdesign@gmail.com>
2023-08-04 19:57:34 -0400

    uploaded files

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -5,8 +5,17 @@ import Product from '../models/productModel.js';
 // @route   GET /api/products
 // @access  Public
 const getProducts = asyncHandler(async (req, res) => {
-    const products = await Product.find({});
-    res.json(products);
+    const pageSize = 8;
+    const page = Number(req.query.pageNumber) || 1;
+
+const keyword = req.query.keyword ? { name: { $regex: req.query.keyword, $options: 'i' } } : {};
+
+    const count = await Product.countDocuments({ ...keyword});
+
+    const products = await Product.find({...keyword})
+    .limit(pageSize)
+    .skip(pageSize * (page - 1));
+    res.json({products, page, pages: Math.ceil(count / pageSize)});
 });
 
 // @dec     Fetch a products
@@ -124,6 +133,14 @@ const createProductReview = asyncHandler(async (req, res) => {
 	}
 });
 
+// @dec     Get top rated products
+// @route   GET /api/products/top
+// @access  Public
+const getTopProducts = asyncHandler(async (req, res) => {
+  const products = await Product.find({}).sort({ rating: -1 }).limit(3);
+  res.status(200).json(products);
+});
+
 export {
     getProducts,
     getProductById,
@@ -131,4 +148,5 @@ export {
     updateProduct,
     deleteProduct,
 		createProductReview,
+    getTopProducts,
 };
